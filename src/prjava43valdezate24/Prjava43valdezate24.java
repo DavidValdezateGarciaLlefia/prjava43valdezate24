e0c20f7c0e663f2476c0a832d48e4c6a287059cd
DavidValdezateGarciaLlefia <valdezategarciadavid@fpllefia.com>
2023-10-22 17:32:09 +0200

    Versió 0.4-B del projecte prjava43valdezate24 Branca master modificada.

--- src/prjava43valdezate24/Prjava43valdezate24.java
+++ src/prjava43valdezate24/Prjava43valdezate24.java
@@ -1,7 +1,9 @@
 package prjava43valdezate24;
 import java.awt.*; // using AWT containers and components
 import java.awt.event.*; // using AWT events and listener interfaces
+import java.net.*;
 // An AWT GUI program inherits the top-level container java.awt.Frame
+
 public class Prjava43valdezate24 extends Frame
 implements ActionListener, WindowListener {
 // This class acts as listener for ActionEvent and WindowEvent
